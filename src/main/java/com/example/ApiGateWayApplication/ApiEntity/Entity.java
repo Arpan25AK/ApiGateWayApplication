f29c6dcfa00b1570a933d29135945991836e5b8d
ArpanAk <akarpan2005@gmail.com>
2026-02-27 18:05:43 +0530

    commit regarding the no orm db creation

--- src/main/java/com/example/ApiGateWayApplication/ApiEntity/Entity.java
+++ src/main/java/com/example/ApiGateWayApplication/ApiEntity/Entity.java
@@ -1,4 +1,11 @@
 package com.example.ApiGateWayApplication.ApiEntity;
 
+import lombok.Data;
+
+@Data
 public class Entity {
+
+    private Long id;
+    private String username;
+    private String password;
 }
