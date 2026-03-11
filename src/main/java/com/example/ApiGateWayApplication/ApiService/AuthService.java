7b74b14e1330f2c66d821a04689f0383ecbb4026
ArpanAk <akarpan2005@gmail.com>
2026-03-11 19:03:25 +0530

    made some api changes

--- src/main/java/com/example/ApiGateWayApplication/ApiService/AuthService.java
+++ src/main/java/com/example/ApiGateWayApplication/ApiService/AuthService.java
@@ -2,6 +2,7 @@ package com.example.ApiGateWayApplication.ApiService;
 
 import com.example.ApiGateWayApplication.ApiEntity.User;
 import com.example.ApiGateWayApplication.ApiRepo.UserRepo;
+import com.example.ApiGateWayApplication.ApiSecurity.JwtUtill;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +13,12 @@ public class AuthService {
 
     private final UserRepo userRepo;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtill jwtUtill;
 
-    public AuthService(UserRepo userRepo, PasswordEncoder passwordEncoder){
+    public AuthService(UserRepo userRepo, PasswordEncoder passwordEncoder, JwtUtill jwtUtill){
         this.userRepo = userRepo;
         this.passwordEncoder = passwordEncoder;
+        this.jwtUtill = jwtUtill;
     }
 
     public String registerUser(String username, String rawPassword){
@@ -33,4 +36,23 @@ public class AuthService {
 
         return "User registered successfully!";
     }
+
+    public String login(String username , String rawpassword){
+
+        Optional<User> existingUser = userRepo.findByUsername(username);
+
+        if(existingUser.isPresent()) {
+            User user = existingUser.get();
+
+            if (passwordEncoder.matches(rawpassword, user.getPassword())) {
+
+                return jwtUtill.generateToken(username);
+            } else {
+                return "Error : Invalid Password";
+            }
+        }else{
+            return "Error: User Not Found!";
+            }
+    }
+
 }
